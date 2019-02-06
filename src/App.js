bac76b8dc92a11f498a23ab7f380c2e4c3835b7d
Mark <hisshash@gmail.com>
2019-02-06 14:09:34 -0500

    Feat (scrollTop): function to scrollTop on page reload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import ScrollTop from './Services/Config/ScrollTop'
 import Landing from './Scenes/Landing/Landing'
 import Privacy from './Scenes/Legal/Privacy/Privacy'
 import Terms from './Scenes/Legal/Terms/Terms'
@@ -8,15 +9,17 @@ import Footer from './Components/Footer/Footer'
 class App extends Component {
   render() {
     return (
-      <main>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/legal/privacy" component={Privacy} />
-          <Route exact path="/legal/terms" component={Terms} />
-        </Switch>
+      <ScrollTop>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/legal/privacy" component={Privacy} />
+            <Route exact path="/legal/terms" component={Terms} />
+          </Switch>
+        </main>
 
         <Footer />
-      </main>
+      </ScrollTop>
     )
   }
 }
