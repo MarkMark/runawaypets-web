8c43af6cf698c5c400a89e9a1680b364c5bb64a3
Mark <hisshash@gmail.com>
2019-02-07 18:14:50 -0500

    Feat (WebP): convert images to webp

--- webpConvert.js
+++ webpConvert.js
@@ -1,7 +1,12 @@
 const imagemin = require('imagemin')
 const imageminWebp = require('imagemin-webp')
 
-imagemin(['src/**/*.{jpg,png}'], './', {
+/**
+ * npm run webp
+ * Convert images {jpg,png} to webp
+ * @returns - ./webp folder with webp`s inside
+ */
+imagemin(['src/**/*.{jpg,png}'], './webp', {
   use: [imageminWebp({ quality: 50 })]
 })
   .then(() => {
